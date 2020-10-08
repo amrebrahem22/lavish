8e5491b2ef62068a7a4251bd32f39c861ad6f886
amrebrahem226 <amrebrahem226@gmail.com>
2020-10-08 18:45:45 +0200

    convert home FBV to Class Based View

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from .models import Room
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    model = Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = 'created'
+    context_object_name = 'rooms'
+    template_name = 'rooms/home.html'
