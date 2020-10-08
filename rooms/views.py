55ce5da05b4d12556ba8553acf6b98bfe49f3761
amrebrahem226 <amrebrahem226@gmail.com>
2020-10-08 07:22:08 +0200

    create the home page with django pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,14 @@
-from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from .models import Room
 
 
 def all_rooms(request):
-    page = request.GET.get('page', 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    rooms = Room.objects.all()[offset:limit]
-    page_count = ceil(Room.objects.count() / page_size)
-    context = {
-        'rooms': rooms, 
-        'page_count': page_count, 
-        'page': page,
-        'page_range': range(0, page_count)
-    }
-    return render(request, 'rooms/home.html', context)
+    page = request.GET.get("page", 1)
+    room_list = Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
