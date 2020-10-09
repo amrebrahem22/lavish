10b7d9cc77834c38ff5d9d4e3f02db8f0d5bee4b
amrebrahem226 <amrebrahem226@gmail.com>
2020-10-09 08:22:00 +0200

    create the search page

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomDetail
+from .views import RoomDetail, search
 
 app_name = 'rooms'
 
 urlpatterns = [
     path('<int:pk>/', RoomDetail.as_view(), name='detail'),
+    path('search/', search, name='search'),
 ]
