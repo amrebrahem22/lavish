774c8974ea99a9acf22f48d8c19801528e9c889c
amrebrahem226 <amrebrahem226@gmail.com>
2020-10-11 04:05:05 +0200

    create the signup page

--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
+import uuid
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 
 GENDER = [
     ('male', 'Male'),
@@ -24,7 +29,27 @@ class User(AbstractUser):
     bio = models.TextField(null=True, blank=True)
     birthdate = models.DateField(null=True)
     language = models.CharField(
-        choices=LANGUAGE, null=True, blank=True, max_length=2)
+        choices=LANGUAGE, null=True, blank=True, max_length=2, default=LANGUAGE[0])
     currency = models.CharField(
-        choices=CURRENCY, null=True, blank=True, max_length=3)
+        choices=CURRENCY, null=True, blank=True, max_length=3, default=CURRENCY[0])
     superhost = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=20, default="", blank=True)
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
+            send_mail(
+                "Verify Lavish Account",
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+            self.save()
+        return
